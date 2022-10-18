f4280efb722336da3205dacb4d3e453cdbe8bacd
Maxime CORNET <maximecornet@outlook.fr>
2022-10-18 17:46:38 +0200

    Adding snc redis

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
